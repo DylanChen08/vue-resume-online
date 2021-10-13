eb320076356dc0ee4d12ffcae10677997bc41b78
DC <easetreychen@gmail.com>
2021-10-13 20:34:25 +0800

    deploy

---
+++ precache-manifest.1821e8d389b9bf69773eb005afde2031.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a22c3c3f1dfc7eaa9d",
+    "url": "/vue-resume-online/css/about.9f74a1e8.css"
+  },
+  {
+    "revision": "bf28114517f8ff49744f",
+    "url": "/vue-resume-online/css/app.7e66a0c6.css"
+  },
+  {
+    "revision": "b7b7cecf3acd1d21e2ac5278133d6a47",
+    "url": "/vue-resume-online/img/avatar.b7b7cecf.png"
+  },
+  {
+    "revision": "e335dbcf86dc217b97ea68f3211e226e",
+    "url": "/vue-resume-online/index.html"
+  },
+  {
+    "revision": "f8a22c3c3f1dfc7eaa9d",
+    "url": "/vue-resume-online/js/about.9a2217c9.js"
+  },
+  {
+    "revision": "bf28114517f8ff49744f",
+    "url": "/vue-resume-online/js/app.7c71355b.js"
+  },
+  {
+    "revision": "182b66d5bccb0a37e9c1",
+    "url": "/vue-resume-online/js/chunk-vendors.b10fcff1.js"
+  },
+  {
+    "revision": "035d1b0a98fe2e1479bda0e8e55920fd",
+    "url": "/vue-resume-online/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-resume-online/robots.txt"
+  }
+]);
